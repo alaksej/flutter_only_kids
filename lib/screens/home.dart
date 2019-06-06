ffe7e32de1fb61ae2f52c6489aa3e81ac193e9e2
Aliaksei Yankou <yankouav@gmail.com>
2019-06-06 20:10:28 +0300

    remove comments

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,16 +14,12 @@ class MyHomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(title),
       ),
       body: BlocBuilder(
           bloc: _counter,
           builder: (BuildContext context, int count) {
             return Center(
-              // Center is a layout widget. It takes a single child and positions it
-              // in the middle of the parent.
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
