e802ca99039b890bc12f1ec249358f890d2ca851
Aliaksei Yankou <yankouav@gmail.com>
2019-08-20 23:35:39 +0300

    add items to the contacts page

--- lib/screens/contacts_page.dart
+++ lib/screens/contacts_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class ContactsPage extends StatelessWidget {
   @override
@@ -10,7 +11,31 @@ class ContactsPage extends StatelessWidget {
       body: Container(
         padding: const EdgeInsets.all(32.0),
         alignment: Alignment.center,
-        child: Text('Only Kids Gomel'),
+        child: ListView(
+          children: <Widget>[
+            Text(
+              'Only Kids Gomel',
+              style: Theme.of(context).textTheme.headline,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: Icon(Icons.location_on),
+                title: Text('Ulitsa Rogachovskaya 2a, Homieĺ 246000'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: SvgPicture.asset(
+                  'assets/instagram.svg',
+                  height: 30.0,
+                ),
+                title: Text('Instagram'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
